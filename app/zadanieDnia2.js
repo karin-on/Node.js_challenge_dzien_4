2b3fab73762473055afb40eb6553d1c0c56e8ced
Karina Nowicka <knowicka@gmail.com>
2018-12-29 13:21:08 +0100

    solved

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const userAgent = req.headers['user-agent'];
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(userAgent);
+});
+
+srv.listen(3000, () => {
+    console.log('zalukaj na port 3000');
+});
